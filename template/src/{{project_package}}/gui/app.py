a99b65d7c5741886fc83d744ab468d9732f56264
Garrett HE <garrett.he@outlook.com>
2024-06-07 10:33:20 +0800

    feat(template): create simplest application with wxpython

---
+++ template/src/{{project_package}}/gui/app.py
@@ -0,0 +1,18 @@
+import wx
+
+from .forms import MainForm
+
+
+class App(wx.App):
+    main_form: MainForm
+
+    def __init__(self):
+        super().__init__(useBestVisual=True)
+
+        self.main_form = MainForm(None)
+
+    def run(self):
+        self.main_form.Show(True)
+        self.main_form.Centre()
+
+        self.MainLoop()
