306878376c8db233e8f1bfaa2a569db65f4eaf23
Garrett HE <garrett.he@outlook.com>
2024-06-07 14:18:35 +0800

    feat(template): add application icons

--- template/src/{{project_package}}/gui/forms/main_form.py
+++ template/src/{{project_package}}/gui/forms/main_form.py
@@ -1,5 +1,9 @@
 from .templates import MainFormTemplate
+from ..res.images import app_icon
 
 
 class MainForm(MainFormTemplate):
-    ...
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.SetIcon(app_icon.GetIcon())
